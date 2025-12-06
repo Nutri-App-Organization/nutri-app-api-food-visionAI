66779d577bcbe59709683ede8db60bcb64d36d48
EnzoLivelli2000 <58576353+EnzoLivelli2000@users.noreply.github.com>
2025-12-06 02:34:15 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,4 @@
 import app from "../index.js";
 import serverless from "serverless-http";
 
-export default async function handler(req, res) {
-  return serverless(app)(req, res);
-}
+export default serverless(app);
